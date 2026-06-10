b0b2890cc1fadd0c80fc0c628adfdae98680e926
Olufunmilola <92426803+Oolufunmi@users.noreply.github.com>
2026-06-10 16:26:18 +0100

    Update day-12.sql

--- day-12.sql
+++ day-12.sql
@@ -2,11 +2,6 @@
 -- Title: North Pole Network Most Active Users
 -- Difficulty: hard
 --
--- Question:
--- The North Pole Network wants to see who's the most active in the holiday chat each day. Write a query to count how many messages each user sent, then find the most active user(s) each day. If multiple users tie for first place, return all of them.
---
--- The North Pole Network wants to see who's the most active in the holiday chat each day. Write a query to count how many messages each user sent, then find the most active user(s) each day. If multiple users tie for first place, return all of them.
---
 
 -- Table Schema:
 -- Table: npn_users
@@ -18,6 +13,10 @@
 --   sender_id: INT
 --   sent_at: TIMESTAMP
 --
+-- Question:
+-- The North Pole Network wants to see who's the most active in the holiday chat each day. Write a query to count how many messages each user sent, then find the most active user(s) each day. 
+--If multiple users tie for first place, return all of them.
+
 
 -- My Solution:
 
