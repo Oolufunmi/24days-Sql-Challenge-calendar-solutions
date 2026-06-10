a36a7e5fd936403018e765ed302f7db5c9dbd202
Olufunmilola <92426803+Oolufunmi@users.noreply.github.com>
2026-06-10 16:26:57 +0100

    Update day-12.sql

--- day-12.sql
+++ day-12.sql
@@ -14,7 +14,8 @@
 --   sent_at: TIMESTAMP
 --
 -- Question:
--- The North Pole Network wants to see who's the most active in the holiday chat each day. Write a query to count how many messages each user sent, then find the most active user(s) each day. 
+-- The North Pole Network wants to see who's the most active in the holiday chat each day. 
+--Write a query to count how many messages each user sent, then find the most active user(s) each day. 
 --If multiple users tie for first place, return all of them.
 
 
